414bcc359b56a35a60a2da26c1c23d6d319b882f
Fernando <fcaballero499@gmail.com>
2020-07-09 16:13:49 -0400

    se agrega la opvion de selccionar el club a apostar (vista primer partido)

--- app/src/main/java/com/example/apostamos/SeleccionPrimerPartido.java
+++ app/src/main/java/com/example/apostamos/SeleccionPrimerPartido.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,6 +28,7 @@ import java.util.Map;
 public class SeleccionPrimerPartido extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private EditText et_monto;
+    private RadioButton rb_club1,rb_club2;
     private ListView lv_apuestas;
     private ListadePartidos Item;
     private ImageView club1, club2;
@@ -40,10 +43,14 @@ public class SeleccionPrimerPartido extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         club1 = findViewById(R.id.imageView);
         club2 = findViewById(R.id.imageView3);
+        rb_club1 = findViewById(R.id.radioButton);
+        rb_club2 = findViewById(R.id.radioButton2);
 
         Item = (ListadePartidos) getIntent().getSerializableExtra("partidoSeleccionado");
         club1.setImageResource(Item.getImagen1());
         club2.setImageResource(Item.getImgen1());
+        rb_club1.setText(Item.getClub1());
+        rb_club2.setText(Item.getClub2());
 
         nAadaptador = new NAadaptador(this,listas);
         lv_apuestas.setAdapter(nAadaptador);
@@ -72,21 +79,37 @@ public class SeleccionPrimerPartido extends AppCompatActivity {
     public void apostar(View view){
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if(user != null){
-                String uid = user.getUid();
-                String email =  user.getEmail();
-                String name = user.getDisplayName();
-                String monto = et_monto.getText().toString();
+            String uid = user.getUid();
+            String email =  user.getEmail();
+            String name = user.getDisplayName();
+            String monto = et_monto.getText().toString();
+            String caso1 = rb_club1.getText().toString();
+            String caso2 = rb_club2.getText().toString();
 
+            if (rb_club1.isChecked()){
                 Map<String, Object> map = new HashMap<>();
                 map.put("usuario", name);
                 map.put("email", email);
                 map.put("monto", monto);
+                map.put("club", caso1);
                 mDatabase.child("PrimerPartido").push().setValue(map);
                 Toast.makeText(this,"La apuesta esta hecha, Mucha suerte.",Toast.LENGTH_LONG).show();
                 et_monto.setText("");
+            }else
+                if(rb_club2.isChecked()){
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("usuario", name);
+                    map.put("email", email);
+                    map.put("monto", monto);
+                    map.put("club", caso2);
+                    mDatabase.child("PrimerPartido").push().setValue(map);
+                    Toast.makeText(this,"La apuesta esta hecha, Mucha suerte.",Toast.LENGTH_LONG).show();
+                    et_monto.setText("");
+                }else {
+                    Toast.makeText(this,"Seleccione un equipo",Toast.LENGTH_SHORT).show();
+                }
             }else {
             Toast.makeText(this,"Debe iniciar sesion",Toast.LENGTH_SHORT).show();
         }
     }
-
 }
